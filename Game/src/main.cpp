cee1a2ec2b6e1ba2920568cb7d5f9242bab4f41d
Mislav <krofnica996@gmail.com>
2012-02-06 12:32:16 +0100

    Copyleft notice

--- Game/src/main.cpp
+++ Game/src/main.cpp
@@ -12,20 +12,20 @@ int main()
 } 
 */
 /*
-    This file is part of Foobar.
+    This file is part of Game Project.
 
-    Foobar is free software: you can redistribute it and/or modify
+    Game Project is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    Foobar is distributed in the hope that it will be useful,
+    Game Project is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+    along with Game Project.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "game.h"
 #include "database.h"
